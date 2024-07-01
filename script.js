b69b7a2638a0bebf8854e49135a0c0601d695bcb
Kanako61 <kana.v3v.moco@gmail.com>
2024-07-01 16:00:30 +0900

    テストアップ

---
+++ script.js
@@ -0,0 +1,6 @@
+(function () {
+    const fs = document.createElement("script");
+    fs.setAttribute("src", "https://cdn.facil-iti.app/tags/faciliti-tag.min.js");
+    fs.dataset.applicationIdentifier = "1e63deac-7eaa-4d33-aa5a-ab94ad7f86c5";
+    document.head.appendChild(fs);
+  }());
